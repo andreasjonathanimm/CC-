994d72534ea8f7cf5bacd4d24509929d7fd8bbd6
Andreas Jonathan Imm <jojonathanimmanuel@gmail.com>
2023-01-30 19:34:09 +0700

    Initial commit

---
+++ halo.c
@@ -0,0 +1,11 @@
+/* File: halo.c */
+/* Penulis: Jonathan Immanuel */
+/* Program menuliskan "Hello, World!" dan "end-of-line" (ganti baris) */
+
+#include<stdio.h>
+int
+main(void) {
+    printf("Hello, world!\n");
+    printf("What's up, world?\n");
+    printf("How is the sky?\n");
+}
